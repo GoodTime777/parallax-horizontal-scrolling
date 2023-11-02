afea2f8509e53838cb49b9473867f0f225272aee
Olga Titova <ots231088@gmail.com>
2023-11-02 20:57:37 +0300

    First commit
    First commit

---
+++ src/scroll.js
@@ -0,0 +1,11 @@
+new Swiper(".slider", {
+  //direction:"vertical",
+  //freeMode:true,
+  speed: 2400,
+  mousewheel: {
+    enabled: true,
+    sensitivity: 2.4,
+  },
+  parallax: true,
+  spaceBetween: 18,
+});
